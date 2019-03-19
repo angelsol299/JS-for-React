999002f46272b6ed37a140905f92b48ce35cb1ed
angelsol299 <angelsol299@hotmail.com>
2019-03-19 14:43:19 +0100

    objects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+const person = {
+  name: "angel",
+  sport() {},
+  talk() {}
+};
+
+person.sport();
+person.name = "";
+const targetMember = "name";
+person[targetMember.value] = "mario";
+
+/*
+//var => function
+//let => block
+//const => block
+
 function sayHello() {
   for (var i = 0; i < 5; i++) {
     console.log(i);
@@ -6,3 +22,4 @@ function sayHello() {
 }
 
 sayHello();
+*/
