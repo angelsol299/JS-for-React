b141c0ff886265f36e799746b2f9d1133e510c6f
angelsol299 <angelsol299@hotmail.com>
2019-03-19 15:28:32 +0100

    objects filtering

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false }
+];
+
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive;
+});
+
+const unactiveJobs = jobs.filter(job => {
+  return job.isActive === false;
+});
+
+console.log(unactiveJobs);
+
+/*
+const square = function(number) {
+  return number * number;
+};
+
+const square = number => number--;
+console.log(square(5));
+
+
 const person = {
   name: "angel",
   sport() {},
@@ -11,7 +36,6 @@ person.walk();
 const walk = person.walk.bind(person);
 walk();
 
-/*
 person.sport();
 person.name = "";
 const targetMember = "name";
